fddcc03f18b6bb490e5fd05cc87562d94df68a68
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-03-15 00:52:21 +0500

    2nd

--- app/src/main/java/com/example/signupactivity/FragmentSignIn.java
+++ app/src/main/java/com/example/signupactivity/FragmentSignIn.java
@@ -1,7 +1,48 @@
 package com.example.signupactivity;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class FragmentSignIn extends Fragment {
+public class FragmentSignIn extends Fragment implements View.OnClickListener {
 
+    EditText mEmail, mPassword;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.sign_in, container, false);
+
+        Button mSignIn = view.findViewById(R.id.signin_button);
+        TextView mWarn1 = view.findViewById(R.id.warn1_textview);
+        mEmail = view.findViewById(R.id.email_edittext);
+        mPassword = view.findViewById(R.id.password_edittext);
+
+        mSignIn.setOnClickListener(this);
+        mWarn1.setOnClickListener(this);
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId())
+        {
+            case R.id.signin_button:
+                break;
+            case R.id.warn1_textview:
+                break;
+        }
+
+    }
 }
--- app/src/main/java/com/example/signupactivity/MainActivity.java
+++ app/src/main/java/com/example/signupactivity/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.signupactivity;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    TextView mMATextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        mMATextView = findViewById(R.id.mainActivity_textview);
+
+
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if(mAuth.getCurrentUser() != null)
+        {
+            mMATextView.setVisibility(View.GONE);
+            FragmentSignUp mSignUp = new FragmentSignUp();
+            fragmentTransaction.replace(R.id.fragment_container, mSignUp);
+            fragmentTransaction.commit();
+
+        }
+        else
+        {
+            mMATextView.setVisibility(View.GONE);
+            FragmentSignIn mSignIn = new FragmentSignIn();
+            fragmentTransaction.replace(R.id.fragment_container, mSignIn);
+            fragmentTransaction.commit();
+
+        }
+    }
 }
