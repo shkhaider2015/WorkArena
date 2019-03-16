17c8b8285e2a4ba6619fb31bb786efa08f71b30e
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-03-16 12:02:11 +0500

    3rd

--- app/src/main/java/com/example/signupactivity/FragmentSignIn.java
+++ app/src/main/java/com/example/signupactivity/FragmentSignIn.java
@@ -4,16 +4,27 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FragmentSignIn extends Fragment implements View.OnClickListener {
 
+    private static final String TAG = "FragmentSignIn";
     EditText mEmail, mPassword;
+    FirebaseAuth mAuth;
 
     @Nullable
     @Override
@@ -25,6 +36,8 @@ public class FragmentSignIn extends Fragment implements View.OnClickListener {
         TextView mWarn1 = view.findViewById(R.id.warn1_textview);
         mEmail = view.findViewById(R.id.email_edittext);
         mPassword = view.findViewById(R.id.password_edittext);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mSignIn.setOnClickListener(this);
         mWarn1.setOnClickListener(this);
@@ -45,4 +58,66 @@ public class FragmentSignIn extends Fragment implements View.OnClickListener {
         }
 
     }
+
+
+    private void userInfo()
+    {
+        String email, password;
+
+        email = mEmail.getText().toString().trim();
+        password = mPassword.getText().toString().trim();
+
+        if(email.isEmpty())
+        {
+            mEmail.setError(getString(R.string.email_required));
+            mEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            mEmail.setError(getString(R.string.email_incorrect));
+            mEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty())
+        {
+            mPassword.setError(getString(R.string.password_required));
+            mPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6)
+        {
+            mPassword.setError(getString(R.string.password_length));
+            mPassword.requestFocus();
+            return;
+        }
+
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        Log.d(TAG, "Sign in onComplete : " + task);
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(getContext(), "Sign in Successfully ", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        Log.d(TAG, "Sign in onFailure : " + e.getMessage());
+                        Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+    }
+
+
 }
--- app/src/main/java/com/example/signupactivity/FragmentSignUp.java
+++ app/src/main/java/com/example/signupactivity/FragmentSignUp.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class FragmentSignUp extends Fragment {
+public class FragmentSignUp extends Fragment implements View.OnClickListener {
+
+    EditText mFullName, mEmail, mPassword, mPhone;
 
     @Nullable
     @Override
@@ -16,6 +22,94 @@ public class FragmentSignUp extends Fragment {
 
         View view = inflater.inflate(R.layout.sign_up, container, false);
 
+        Button mSignUp = view.findViewById(R.id.signup_button);
+        TextView mWarn2 = view.findViewById(R.id.warn2_textview);
+        mFullName = view.findViewById(R.id.fullname_edittext);
+        mEmail = view.findViewById(R.id.email_edittext);
+        mPassword = view.findViewById(R.id.password_edittext);
+        mPhone = view.findViewById(R.id.phone_edittext);
+
+        mSignUp.setOnClickListener(this);
+        mWarn2.setOnClickListener(this);
+
         return view;
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.signup_button:
+                break;
+            case R.id.warn2_textview:
+                break;
+        }
+
+    }
+
+    private void userInfo()
+    {
+        String fullName , email, password, phone;
+
+        fullName = mFullName.getText().toString().trim();
+        email = mEmail.getText().toString().trim();
+        password = mPassword.getText().toString().trim();
+        phone = mPhone.getText().toString().trim();
+
+        if(fullName.isEmpty())
+        {
+            mFullName.setError(getString(R.string.fullname_required));
+            mFullName.requestFocus();
+            return;
+        }
+        if(fullName.length() > 20)
+        {
+            mFullName.setError(getString(R.string.fullname_length));
+            mFullName.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty())
+        {
+            mEmail.setError(getString(R.string.email_required));
+            mEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            mEmail.setError(getString(R.string.email_incorrect));
+            mEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty())
+        {
+            mPassword.setError(getString(R.string.password_required));
+            mPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6)
+        {
+            mPassword.setError(getString(R.string.password_length));
+            mPassword.requestFocus();
+            return;
+        }
+        if(phone.isEmpty())
+        {
+            mPhone.setError(getString(R.string.phone_required));
+            mPhone.requestFocus();
+            return;
+        }
+        if(phone.length() != 11)
+        {
+            mPhone.setError(getString(R.string.phone_length));
+            mPhone.requestFocus();
+            return;
+        }
+
+    }
+
+
+
 }
