a0b81aa2507ce3d5a32f92b21a47772c1b7da2da
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-03-17 16:30:18 +0500

    4th

--- app/src/main/java/com/example/signupactivity/HomeActivity.java
+++ app/src/main/java/com/example/signupactivity/HomeActivity.java
@@ -1,6 +1,10 @@
 package com.example.signupactivity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,10 +12,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener , NavigationView.OnNavigationItemSelectedListener {
 
     FirebaseAuth mAuth;
 
@@ -25,11 +30,25 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         mAuth = FirebaseAuth.getInstance();
 
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
 
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View v)
+    {
 
     }
 
@@ -57,6 +76,22 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
 
+
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_profile:
+                Toast.makeText(this, "Profile Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_portfolio:
+                Toast.makeText(this, "Portfolio Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
 
         return true;
     }
